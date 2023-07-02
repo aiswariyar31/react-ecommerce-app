a325f63bc643f42cb301f659228a18d6c92f5500
Aiswariya K <k.aiswariyak>
2023-07-02 08:02:14 -0700

    first commit

---
+++ src/components/Logout/Logout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Logout.css'
+
+function Logout() {
+  return (
+    <div className='logout-container'>
+        <div className='logout-content'>
+            <h1>You are successfully logged out</h1>
+            <p>Thank you</p>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Logout
